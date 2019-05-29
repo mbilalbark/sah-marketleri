4cb8fdb1ff7d914e97208bb95faaefb493da90f7
mbilalbark <2012010217031@ogrenci.karabuk.edu.tr>
2019-05-29 06:19:10 +0300

    Add: Market add

--- controller.php
+++ controller.php
@@ -12,8 +12,16 @@ if ($conn->connect_error) {
 } 
 
  function magaza() {
-    $sql = "SELECT id, name, address, created_at,closed_at FROM store";
+    $sql = "SELECT id, title, address, created_at,closed_at FROM store";
     $result =  $GLOBALS['conn']->query($sql);
+    echo "
+    <div class='my-2'></div>
+    <a href='index.php?metot=yeni-magaza-index' class='btn btn-success btn-icon-split'>
+    <span class='icon text-white-50'>
+    <i class='fas fa-plus-square'></i>
+    </span>
+    <span class='text'>Yeni Mağaza</span>
+  </a>";
   echo " <div class='card-body'>
     <div class='table-responsive'>
       <table class='table table-bordered' id='dataTable' width='100%' cellspacing='0'>
@@ -34,7 +42,7 @@ if ($result->num_rows > 0) {
         echo    "
         <tr>
           <td> " .$row["id"]. " </td>
-          <td>" . $row["name"]. "</td>
+          <td>" . $row["title"]. "</td>
           <td>" . $row["address"]. "</td>
           <td>". $row["created_at"]. "</td>
           <td>". $row["closed_at"]. "</td>
@@ -56,4 +64,28 @@ echo "
      echo "personel kayıtları burada başkan";
  }
 
+ function yeniMagazaIndex() {
+   echo "
+    <h2> Yeni Mağaza Ekleme Formu </h2> 
+   <form class='user' action='index.php' method='post'>
+   <div class='form-group'>
+     <input type='Text' class='form-control form-control-user' id='exampleInputEmail' name='name' aria-describedby='emailHelp' placeholder='Magaza ismi'>
+   </div>
+   <div class='form-group'> 
+     <input type='Text' class='form-control form-control-user' id='exampleInputEmail' name='address' aria-describedby='emailHelp' placeholder='Adres'>
+  </div>
+   <input  type='submit' name='yeniMagaza' value='Ekle' class='btn btn-primary btn-user btn-block'/ >
+   <hr>
+ </form>";
+ }
+function yeniMagaza($name1, $address) {
+  $dateNow = date('Y/m/d');
+  $sql = "INSERT INTO store (title, address, created_at) VALUES ('$name1', '$address', '$dateNow' )";
+
+if ($GLOBALS['conn']->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" .$GLOBALS['conn']->error;
+}
+}
 ?>
